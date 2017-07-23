8988441b1e2c1c41ac07ae97b49879ba872337c7
Pradeep Mudaliar <mudaliarpradeep@gmail.com>
2017-07-23 17:29:20 +0530

    class reformatted

--- src/main/java/com/techedgeco/Bootstrap.java
+++ src/main/java/com/techedgeco/Bootstrap.java
@@ -5,7 +5,7 @@ import static spark.Spark.get;
 import spark.Spark;
 
 public class Bootstrap {
-	 private static final String IP_ADDRESS = System.getenv("OPENSHIFT_DIY_IP") != null ? System.getenv("OPENSHIFT_DIY_IP") : "localhost";
+	 	private static final String IP_ADDRESS = System.getenv("OPENSHIFT_DIY_IP") != null ? System.getenv("OPENSHIFT_DIY_IP") : "localhost";
 	    private static final int PORT = System.getenv("OPENSHIFT_DIY_PORT") != null ? Integer.parseInt(System.getenv("OPENSHIFT_DIY_PORT")) : 8080;
 	 
 	    public static void main(String[] args) throws Exception {
